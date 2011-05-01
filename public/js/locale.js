8f26d29edc222a0fea655beebcda76c07bb9c07f
Iceberg Young <iceberg.young@gmail.com>
2011-05-01 18:20:50 +0800

    Add localize entries, update demo strings beside.

--- public/js/locale.js
+++ public/js/locale.js
@@ -5,7 +5,7 @@ window.POTATO_L10N = {
 		// header
 		// search bar
 		search : 'Search',
-		search_in_potato : 'Search in Potato',
+		search_in_potato : 'Plough Potato',
 		// left panel
 		item_more : 'Seed',
 		page_first : 'First',
@@ -17,11 +17,18 @@ window.POTATO_L10N = {
 		season_summer : 'Summer',
 		season_autumn : 'Autumn',
 		season_winter : 'Winter',
-		//
+		// main panel
 		details_depot : 'Depot',
-		//
+		detail_season : 'Season',
+		detail_variety : 'Variety',
+		detail_weight : 'Weight',
+		detail_maturity : 'Maturity',
+		detail_description : 'Description',
+		detail_comments : 'Comments',
+		// right panel
 		statistics : 'Statistics',
 		// footer
+		version : '1.0.0.0',
 		copyright : 'Copyright &copy; 2011 Bears Home'
 	}
 };
