16d01bdc64d4e2d2b5ea49083bf0e2236c16d547
Iceberg Young <iceberg.young@gmail.com>
2011-09-01 23:45:18 +0800

    Implement insert of individual.

--- script/database/individual.class.php
+++ script/database/individual.class.php
@@ -7,7 +7,12 @@ namespace database;
 abstract class individual {
 
 	public function __construct() {
-		self::sampling( get_object_vars( $this ) );
+		if ( isset( $this->uuid ) ) {
+			self::sampling( $this );
+		}
+		else {
+			$this->lock = 0;
+		}
 	}
 
 	/**
@@ -19,21 +24,46 @@ abstract class individual {
 	}
 
 	/**
-	 * Update this object to datebase.
+	 * Insert/update this object to database.
+	 * @return boolean
 	 */
-	public function update() {
-		$type = str_replace( '\\', '.', self::domain() );
-		$query = self::update_query( $type );
-		$query->execute( array( ':uuid' => $this->uuid ) );
+	public function save() {
+		$exist = isset( $this->uuid );
+		if ( $exist ) {
+			$query = self::update_query();
+			$result = $query->execute( array( ':uuid' => $this->uuid ) );
+		}
+		else {
+			$query = self::insert_query( $this, $properties );
+			$result = $query->execute( $properties );
+		}
+		if ( $result ) {
+			foreach ( $query->fetch() as $name => $value ) {
+				$this->$name = $value;
+			}
+			$query->closeCursor();
+		}
+		else {
+			$error = $query->errorInfo();
+			throw new \exception\failed_store( $error[2], $error[0] );
+		}
+		if ( !$exist ) {
+			self::$object_pool[$this->key()] = $this;
+		}
+		self::sampling( $this );
+		return $result;
 	}
 
 	/**
 	 * Delete this object from database.
+	 * @return boolean
 	 */
 	public function delete() {
-		$type = str_replace( '\\', '.', self::domain() );
-		$query = self::delete_query( $type );
-		$query->execute( array( ':uuid' => $this->uuid ) );
+		$query = self::delete_query();
+		$result = $query->execute( array( ':uuid' => $this->uuid ) );
+		$key = $this->key();
+		unset( self::$object_pool[$key], self::$sample_pool[$key] );
+		return $result;
 	}
 
 	/**
@@ -46,45 +76,31 @@ abstract class individual {
 		$key = "$domain#$uuid";
 		if ( !isset( self::$object_pool[$key] ) ) {
 			$query = self::select_query( $domain );
-			$query->execute( array( ':uuid' => $uuid ) );
-			self::$object_pool[$key] = $query->fetch();
-			$query->closeCursor();
+			if ( $query->execute( array( ':uuid' => $uuid ) ) ) {
+				self::$object_pool[$key] = $query->fetch();
+				$query->closeCursor();
+			}
+			else {
+				throw new \exception\unkown_object();
+			}
 		}
 		return self::$object_pool[$key];
 	}
 
 	/**
-	 * Get prepared query to select data.
-	 * @param string $type
-	 * @return PDOStatement
+	 * Get cache key of this object.
+	 * @return string
 	 */
-	private static function select_query( $type ) {
-		if ( !isset( self::$select_pool[$type] ) ) {
-			$query = connection::get_pdo()->prepare( "SELECT * FROM $type WHERE uuid=:uuid" );
-			$query->setFetchMode( \PDO::FETCH_CLASS, $type );
-			self::$select_pool[$type] = $query;
-		}
-		return self::$select_pool[$type];
-	}
-
-	/**
-	 * Get prepared query to delete data.
-	 * @param string $type
-	 * @return PDOStatement
-	 */
-	private static function delete_query( $type ) {
-		if ( !isset( self::$delete_pool[$type] ) ) {
-			$query = connection::get_pdo()->prepare( "DELETE FROM $type WHERE uuid=:uuid" );
-			self::$delete_pool[$type] = $query;
-		}
-		return self::$delete_pool[$type];
+	private function key() {
+		return self::domain() . '#' . $this->uuid;
 	}
 
 	/**
 	 * Take trait of an object for conditional updating.
-	 * @param string $key
+	 * @param individual $object
 	 */
-	private static function sampling( $properties ) {
+	private static function sampling( individual $object ) {
+		$properties = get_object_vars( $object );
 		$key = self::domain() . '#' . $properties['uuid'];
 		unset( $properties['uuid'], $properties['lock'] );
 		$samples = array( );
@@ -92,6 +108,57 @@ abstract class individual {
 			$samples[crc32( $name )] = crc32( $value );
 		}
 		self::$sample_pool[$key] = $samples;
+	}
+
+	/**
+	 * Get prepared query to insert data.
+	 * @param individual $object
+	 * @param array $properties [OUT]
+	 * @return PDOStatement
+	 */
+	private static function insert_query( individual $object, &$properties ) {
+		$vars = get_object_vars( $object );
+		unset( $vars['uuid'] );
+		foreach ( $vars as $name => $value ) {
+			$properties[':' . $name] = $value;
+		}
+
+		$domain = self::domain();
+		if ( !isset( self::$insert_pool[$domain] ) ) {
+			$fields = '"uuid","' . implode( '","', array_keys( $vars ) ) . '"';
+			$holder = 'uuid_generate_v4(),' . implode( ',', array_keys( $properties ) );
+			$query = connection::get_pdo()->prepare( "INSERT INTO \"$domain\"($fields) VALUES($holder) RETURNING \"uuid\"" );
+			$query->setFetchMode( \PDO::FETCH_ASSOC );
+			self::$insert_pool[$domain] = $query;
+		}
+		return self::$insert_pool[$domain];
+	}
+
+	/**
+	 * Get prepared query to delete data.
+	 * @return PDOStatement
+	 */
+	private static function delete_query() {
+		$domain = self::domain();
+		if ( !isset( self::$delete_pool[$domain] ) ) {
+			$query = connection::get_pdo()->prepare( "DELETE FROM \"$domain\" WHERE \"uuid\"=:uuid" );
+			self::$delete_pool[$domain] = $query;
+		}
+		return self::$delete_pool[$domain];
+	}
+
+	/**
+	 * Get prepared query to select data.
+	 * @param string $domain
+	 * @return PDOStatement
+	 */
+	private static function select_query( $domain ) {
+		if ( !isset( self::$select_pool[$domain] ) ) {
+			$query = connection::get_pdo()->prepare( "SELECT * FROM \"$domain\" WHERE \"uuid\"=:uuid" );
+			$query->setFetchMode( \PDO::FETCH_CLASS, $domain );
+			self::$select_pool[$domain] = $query;
+		}
+		return self::$select_pool[$domain];
 	}
 
 	/**
@@ -111,9 +178,10 @@ abstract class individual {
 	//@{
 	private static $object_pool = array( );
 	private static $sample_pool = array( );
-	private static $select_pool = array( );
+	private static $insert_pool = array( );
 	private static $update_pool = array( );
 	private static $delete_pool = array( );
+	private static $select_pool = array( );
 	//@}
 }
 ?>
