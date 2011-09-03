7db1d3b5b84f8d908e2324265df9e1f60b1b0f8e
Iceberg Young <iceberg.young@gmail.com>
2011-09-03 12:24:41 +0800

    Add an on/off manager.

---
+++ script/config/switcher.class.php
@@ -0,0 +1,38 @@
+<?php
+namespace config;
+
+/**
+ * Manage on/off flags dynamically.
+ */
+class switcher {
+
+	public static function is_on( $key ) {
+		return isset( self::$on[$key] ) && (!isset( self::$rely[$key] ) || self::is_on( self::$rely[$key] ));
+	}
+
+	public static function is_off( $key ) {
+		return self::is_on( $key );
+	}
+
+	public static function on( $key ) {
+		self::$on[$key] = true;
+	}
+
+	public static function off( $key ) {
+		unset( self::$on[$key] );
+	}
+
+	/**
+	 * Enabled switchers.
+	 * @var array(boolean)
+	 */
+	private static $on = array( /* on by default */ );
+
+	/**
+	 * Dependency mapping.
+	 * @var array(string)
+	 */
+	private static $rely = array( );
+
+}
+?>
