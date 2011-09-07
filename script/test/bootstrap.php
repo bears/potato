604e1a18f94b364e2904be3a7f6baac9eedf6a8b
Iceberg Young <iceberg.young@gmail.com>
2011-09-07 23:33:19 +0800

    Add bootstrap for unit test. Add unit test for connection class.

---
+++ script/test/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+namespace test;
+
+/**
+ * Setup include/load path for unit test.
+ */
+chdir( dirname( __FILE__ ) . '/..' );
+require_once 'handler/loader.php';
---
+++ script/test/script/database/connection.test.php
@@ -0,0 +1,21 @@
+<?php
+namespace test\database;
+
+/**
+ * Test class for \database\connection.
+ */
+class connection extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @covers \database\connection::get_pdo
+	 */
+	public function test_get_pdo() {
+		$instance1 = \database\connection::get_pdo();
+		$this->assertInstanceOf('PDO', $instance1);
+
+		$instance2 = \database\connection::get_pdo();
+		$this->assertInstanceOf('PDO', $instance2);
+
+		$this->assertSame($instance1, $instance2);
+	}
+}
