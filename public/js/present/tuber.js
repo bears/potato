415fc20fe4864d02c483bd2203d0aa5a06a34e8a
Iceberg Young <iceberg.young@gmail.com>
2011-09-17 01:41:43 +0800

    Prevent duplicated pTubers to be created.

--- public/js/present/tuber.js
+++ public/js/present/tuber.js
@@ -4,23 +4,43 @@
  * @param target {String}
  */
 function pTuber(uuid, target) {
+	// Prevent duplicated object.
+	var cached = $('#tuber_' + uuid).data('self');
+	if (cached instanceof pTuber) {
+		return cached;
+	}
+
+	/**
+	 * The element to insert into.
+	 */
 	var vessel = $('#season_' + target + ' .season');
 
+	/**
+	 * Callback for sPotato.
+	 * @param subject {String}
+	 * @param type {String} One of POTATO.NOTIFY.*
+	 * @param source {sPotato}
+	 */
 	this.notify = function(subject, type, source) {
 		switch (type) {
 			case POTATO.NOTIFY.INSERT:
-				$(pTuber.template.replace(/{%(\w+)%}/g, function(unused, key) {
+				var html = pTuber.template.replace(/{%(\w+)%}/g, function(unused, key) {
 					return source.get(key, 'tuber');
-				}).replace('{%$%}', source.uuid())).appendTo(vessel).click(function() {
+				}).replace('{%$%}', source.uuid());
+				$(html).data('self', this).click(function() {
 					$('#seasons li.ui-state-highlight').removeClass('ui-state-highlight');
 					$(this).addClass('ui-state-highlight');
 					//new pDetail(data.id);
-				});
+				}).appendTo(vessel);
 				break;
 		}
 	};
 
+	// Subscribe to the data source.
 	(new sPotato(uuid)).subscribe('tuber', this);
 }
 
+/**
+ * HTML template for new element.
+ */
 pTuber.template = '<li id="tuber_{%$%}" class="ui-widget-content"><a class="handle ui-icon ui-icon-{%brand%}"></a>{%label%}</li>';
