12817ebe61d6682ba73de888071772beb697f0ac
Iceberg Young <iceberg.young@gmail.com>
2011-10-15 12:12:47 +0800

    Add ab.js for client side abbreviation lookup.

---
+++ public/js/subject/ab.js
@@ -0,0 +1,20 @@
+/**
+ * Abbreviation dictionary.
+ */
+POTATO.AB = {
+	potato : {
+		tuber : {
+			$ : 't',
+			brand : 'b',
+			label : 'l'
+		},
+		stock : {
+			$ : 's',
+			season : 'n',
+			weight : 'w',
+			variety : 'v',
+			seeding : 'g',
+			harvest : 'h'
+		}
+	}
+};
